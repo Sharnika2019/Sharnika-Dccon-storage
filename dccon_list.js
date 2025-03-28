86e1dcac66394e6dd11abc70ef5d1ccd63a776ce
Sharnika <68538168+Sharnika2019@users.noreply.github.com>
2025-03-28 15:30:07 +0900

    Delete dccon_list.js

--- dccon_list.js
+++
@@ -1,4 +0,0 @@
-dcConsData = [
-  {"name":"한눈.jpg", "uri":"https://i.postimg.cc/zBTzYZdq/image.jpg", "keywords":["한눈"], "tags":["미지정"]},
-  {"name":"치킨댄스.gif", "uri":"https://i.postimg.cc/pTCC0Y2x/image.gif", "keywords":["치킨댄스"], "tags":["미지정"]}
-];
